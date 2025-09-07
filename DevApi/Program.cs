ea86756db0fcf401261de28e9922d77501a29676
Dev00700 <iamdevendra03@outlook.com>
2025-09-07 12:00:19 +0530

    commit

--- DevApi/Program.cs
+++ DevApi/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
     {
-        policy.WithOrigins("http://localhost:4200", "http://localhost:83", "http://localhost:82", "https://68.178.164.44:82", "http://68.178.164.44:82")
+        policy.WithOrigins("http://localhost:4200", "http://localhost:83", "http://localhost:86", "https://68.178.164.44:82", "http://68.178.164.44:86")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
