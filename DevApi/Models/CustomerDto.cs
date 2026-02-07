2947304aca17ff277c99a5e761c2856699ed434c
Dev00700 <iamdevendra03@outlook.com>
2026-02-07 14:04:03 +0530

    commit

--- DevApi/Models/CustomerDto.cs
+++ DevApi/Models/CustomerDto.cs
@@ -83,6 +83,7 @@ namespace DevApi.Models
     public bool? IsActive { get; set; }
     public string? ImageUrl { get; set; }
     public long? CustomerPlotId { get; set; }
+    public long? CustomerId { get; set; }
     public int PlotStatus { get; set; }
 }
 
